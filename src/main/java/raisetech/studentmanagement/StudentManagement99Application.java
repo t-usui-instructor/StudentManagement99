9412550f837b41dca741d2d7400286ec82e2ea50
t-usui-instructor <tadahiro.usui@idp-k.com>
2025-07-24 14:47:45 +0900

    first commit

---
+++ src/main/java/raisetech/studentmanagement/StudentManagement99Application.java
@@ -0,0 +1,22 @@
+package raisetech.studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudentManagement99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagement99Application.class, args);
+	}
+
+	//curl.exe "http://localhost:8080/hello"
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello,World!!";
+	}
+
+}
---
+++ src/test/java/raisetech/studentmanagement/StudentManagement99ApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagement99ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
